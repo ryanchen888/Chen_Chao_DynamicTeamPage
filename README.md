14cce31c8321c87877dfa7618f561f156fdd4653
CCryan888 <113542854+ryanchen888@users.noreply.github.com>
2023-03-31 12:43:03 -0400

    develop branch
    create codes

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Chen_Chao_DynamicTeamPage
+
+# Dynamic Team build
+
+All about my awesome team members!
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+MIT
---
+++ js/heroes.js
@@ -0,0 +1,13 @@
+let heroes = {
+    Ryan: {
+        name: 'Chen Chao - The leader of the Team',
+        bio: 'Having created the majority of the work, and taught Steve to wear a stylish black turtleneck jumper!',
+        achievement: 'Invented the first Apple computer and gave it to Steve.'
+    },
+
+    Jobs: {
+        name: 'Steve Jobs - The Sidekick',
+        bio: 'Sells computers and is very good at making products thin and light with reduced hardware.',
+        achievement: 'Leaving a pile of broken products and handing the company over to Cook, who is even worse than he is.'
+    },
+}
